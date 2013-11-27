cb81493046be92b84213d7f4eb69d993b2b463e5
C.W. Betts <computers57@hotmail.com>
2013-11-27 16:21:20 -0700

    Initial Commit.

---
+++ Glypha III/GlyMyScene.h
@@ -0,0 +1,13 @@
+//
+//  GlyMyScene.h
+//  Glypha III
+//
+
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GlyMyScene : SKScene
+
+@end
