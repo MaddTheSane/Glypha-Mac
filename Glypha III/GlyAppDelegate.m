91cacd10755fe5518530828ba295f479de82b055
C.W. Betts <computers57@hotmail.com>
2013-11-27 16:55:49 -0700

    Indentation and other fixes.

--- Glypha III/GlyAppDelegate.m
+++ Glypha III/GlyAppDelegate.m
@@ -15,20 +15,22 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    /* Pick a size for the scene */
-    SKScene *scene = [GlyMyScene sceneWithSize:CGSizeMake(1024, 768)];
-
-    /* Set the scale mode to scale to fit the window */
-    scene.scaleMode = SKSceneScaleModeAspectFit;
-
-    [self.skView presentScene:scene];
-
-    self.skView.showsFPS = YES;
-    self.skView.showsNodeCount = YES;
+	/* Pick a size for the scene */
+	SKScene *scene = [GlyMyScene sceneWithSize:CGSizeMake(1024, 768)];
+	
+	/* Set the scale mode to scale to fit the window */
+	scene.scaleMode = SKSceneScaleModeAspectFit;
+	
+	[self.skView presentScene:scene];
+#ifdef DEBUG
+	self.skView.showsFPS = YES;
+	self.skView.showsNodeCount = YES;
+#endif
 }
 
-- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
-    return YES;
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
+{
+	return YES;
 }
 
 @end
